ab1d02d60da058acc5cbe67b84a7ae6e087f977c
changetocoding <david@changetocoding.co.uk>
2020-04-14 13:14:15 +0100

    Week 3 homework

---
+++ Week 3/Homework/LoreFizzBuzz.py
@@ -0,0 +1,15 @@
+#Python.exe C:\Dev\intro\FizzBuzz.py
+
+x = range(1,100)
+
+for x in range(1,100):
+    if (x % 3 == 0 and x % 5 == 0):
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif (x % 5 == 0):
+        print("Buzz")
+    else:
+        print("This is {} out of 100".format(x))
+        
+
